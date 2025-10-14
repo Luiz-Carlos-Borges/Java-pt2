2e2e79c7bfa6591f5335ceff497c4064b21713ad
Luiz Carlos Borges D'Amico <luizcbdamico1@gmail.com>
2025-10-13 21:59:11 -0300

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,32 @@
+import { filmes } from '../data/filmes';
+
+function Home () {
+    console.log(filmes);
+
+    const primeiroFilme = filmes[0];
+
+    const estiloImagem = {
+        width: "300px"
+    };
+
+    return (
+        <>
+            <h1>Filmes</h1>
+
+            <ul>
+                    
+                    <li>
+                                <p>Título: {primeiroFilme.titulo}</p>
+                                <p>Ano: {primeiroFilme.ano}</p>
+                                <p>Gênero: {primeiroFilme.genero}</p>
+                                <p>Nota: {primeiroFilme.nota}</p>
+
+                                <img style = {estiloImagem} src={primeiroFilme.poster}/>
+                    </li>
+                    
+            </ul>
+        </>
+    );
+}
+
+export default Home;
